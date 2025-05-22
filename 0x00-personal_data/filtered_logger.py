378bc5bdb0ee88839af7dad0fa760d159441fa87
Omar Mohumed <wadaniinarais1@gmail.com>
2025-05-22 22:54:18 +0300

    updated filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,101 @@
 #!/usr/bin/env python3
 """
-Provides a function to obfuscate specified fields in a log message.
+Filtered Logger Module
 """
 
+import logging
 import re
+import os
+import mysql.connector
 from typing import List
 
 
 def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str, separator: str) -> str:
-    """Returns the obfuscated log message."""
-    return re.sub(f"({'|'.join(fields)})=.*?{separator}",
-                  lambda m: f"{m.group(1)}={redaction}{separator}", message)
+                 redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Obfuscates the values of specified fields within a log message.
+    """
+    for field in fields:
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original = super().format(record)
+        return filter_datum(self.fields,
+                            self.REDACTION, original,
+                            self.SEPARATOR)
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a configured logger instance
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Connects to MySQL database using credentials from environment variables.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+
+
+def main():
+    """
+    Main function that retrieves and logs obfuscated user data
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    field_names = [i[0] for i in cursor.description]
+    logger = get_logger()
+
+    for row in cursor:
+        message = "; ".join(f"{key}={value}" for key,
+                            value in zip(field_names, row)) + ";"
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
